9729098f047b3e90e6b8068fcef0222738d82e0e
Joao Pedro <joaopedroricarte@hotmail.com>
2023-01-03 17:09:51 -0300

    Update app.component.ts, components.module.ts, and 5 more files...

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -4,33 +4,48 @@ import { Location, LocationStrategy, PathLocationStrategy } from '@angular/commo
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  public focus;
-  public listTitles: any[];
-  public location: Location;
-  constructor(location: Location,  private element: ElementRef, private router: Router) {
-    this.location = location;
-  }
 
-  ngOnInit() {
-    this.listTitles = ROUTES.filter(listTitle => listTitle);
-  }
-  getTitle(){
-    var titlee = this.location.prepareExternalUrl(this.location.path());
-    if(titlee.charAt(0) === '#'){
-        titlee = titlee.slice( 1 );
+    public focus;
+    public theme = false;
+    public listTitles: any[];
+    public location: Location;
+
+    constructor(location: Location, private element: ElementRef, private router: Router) {
+        this.location = location;
+        this.setTheme();
+    }
+    
+    ngOnInit() {
+        this.listTitles = ROUTES.filter(listTitle => listTitle);
     }
 
-    for(var item = 0; item < this.listTitles.length; item++){
-        if(this.listTitles[item].path === titlee){
-            return this.listTitles[item].title;
+    setTheme() {
+        let body = document.getElementById("body");
+        if (this.theme) {
+            body.className = "dark";
+        }
+        else {
+            body.className = "light";
         }
     }
-    return 'Dashboard';
-  }
+
+    getTitle() {
+        var titlee = this.location.prepareExternalUrl(this.location.path());
+        if (titlee.charAt(0) === '#') {
+            titlee = titlee.slice(1);
+        }
+
+        for (var item = 0; item < this.listTitles.length; item++) {
+            if (this.listTitles[item].path === titlee) {
+                return this.listTitles[item].title;
+            }
+        }
+        return 'Dashboard';
+    }
 
 }
